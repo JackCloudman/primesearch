f9eb34fb2d5cec82328a794ee898ba3107d7332f
Maylo-Kun <maylo360xd@gmail.com>
2017-03-17 15:57:30 -0500

    Time for linux

---
+++ primoslinux.c
@@ -0,0 +1,48 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+#include<time.h>
+int numint,total=0,i=3,es_primo,primo,limite;
+float num;
+int main(){
+  //Leemos un numero entero
+  scanf("%f",&num );
+  numint=num;
+  if(num>=2&&(num-numint)==0){
+    clock_t start = clock();
+    int listaprimos[numint/2];
+    listaprimos[0]=2;
+    while(i<=num){
+      limite=((int)pow(num,0.5))+1;
+      es_primo=1;
+      for(primo=0;primo<=total;primo++){
+        if(i%listaprimos[primo]==0){
+          es_primo=0;
+          break;
+        }
+        if(listaprimos[primo]>limite){
+          break;
+        }
+      }
+      if(es_primo){
+        total++;
+        listaprimos[total]=i;
+      }
+      i+=2;
+    }
+    printf("[");
+    for(i=0;i<total;i++){
+      if(listaprimos[i]==0){
+        break;
+      }
+      printf("%d,",listaprimos[i]);
+    }
+    printf("%d",listaprimos[total]);
+    printf("]");
+    printf("\nTiempo: %fs\n",((double)clock()-start)/100000);
+  }
+  else{
+    printf("Debe ser un numero entero mayor o igual a 2!\n");
+  }
+  return 1;
+}
