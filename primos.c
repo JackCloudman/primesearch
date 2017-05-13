2ef4af4ae3db704639c609390cabf2b611b6cf03
Jack Cloudman <maylo360xd@gmail.com>
2017-05-13 02:20:31 -0500

    Add files via upload

--- primos.c
+++ primos.c
@@ -2,7 +2,8 @@
 #include<stdio.h>
 #include<math.h>
 #include<time.h>
-int numint,total=0,i=3,es_primo,primo,limite;
+unsigned int numint,i=3,total=0,primo,limite;
+short es_primo;
 float num;
 int main(){
   //Leemos un numero entero
@@ -10,7 +11,8 @@ int main(){
   numint=num;
   if(num>=2&&(num-numint)==0){
     clock_t start = clock();
-    int listaprimos[numint/2];
+    int *listaprimos;
+    listaprimos=(int*)calloc(1,sizeof(int));
     listaprimos[0]=2;
     while(i<=num){
       limite=((int)pow(num,0.5))+1;
@@ -26,10 +28,13 @@ int main(){
       }
       if(es_primo){
         total++;
+        listaprimos=(int*)realloc(listaprimos,sizeof(int)*(total+1));
         listaprimos[total]=i;
       }
       i+=2;
     }
+    printf("\nCalculado!\nTiempo: %fs\nTotal de numeros primos encontrados:%d\nMayor numero primo encontrado:%d\n",((double)clock()-start)/1000,total+1,listaprimos[total]);
+    system("PAUSE");
     printf("[");
     for(i=0;i<total;i++){
       if(listaprimos[i]==0){
@@ -39,7 +44,6 @@ int main(){
     }
     printf("%d",listaprimos[total]);
     printf("]");
-    printf("\nTiempo: %fs\n",((double)clock()-start)/1000);
   }
   else{
     printf("Debe ser un numero entero mayor o igual a 2!\n");
